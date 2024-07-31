75436909b8cda9bf2754e7a8879e6bf17d32d042
rafael <rhirata.s@gmail.com>
2024-07-31 14:58:38 -0300

    Initial backend commit.

---
+++ clinic-api/src/main/java/com/example/clinic/ClinicApplication.java
@@ -0,0 +1,14 @@
+package com.example.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(ClinicApplication.class, args);
+	}
+
+}
