62af56399f021990e2559a9c5c3c956a61193b2a
rafael <rhirata.s@gmail.com>
2024-08-06 11:05:34 -0300

    Added search component

--- clinic-web/src/app/app.config.ts
+++ clinic-web/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -11,6 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(),
-    provideHttpClient(withFetch()), provideAnimationsAsync(),
+    provideHttpClient(withFetch()), 
+    provideAnimationsAsync(),
   ]
 };
