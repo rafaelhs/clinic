5df521da5fa6563533f13e04cd6925d11faa4ace
rafael <rhirata.s@gmail.com>
2024-08-07 22:53:49 -0300

    Added new tests

--- clinic-api/src/test/java/com/example/clinic/controller/PatientControllerTest.java
+++ clinic-api/src/test/java/com/example/clinic/controller/PatientControllerTest.java
@@ -1,38 +1,250 @@
 package com.example.clinic.controller;
 
+import com.example.clinic.model.Patient;
+import com.example.clinic.repository.PatientRepository;
 import com.example.clinic.service.PatientService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.containsString;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest()
 @AutoConfigureMockMvc
 class PatientControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
+    @Autowired
     private PatientService patientService;
+    @MockBean
+    private PatientRepository patientRepository;
+
+    // Create Tests
+    @Test
+    void shouldCreatePatient() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857003");
+        patient.setEmail("emailTest@test.com");
+
+        String requestBody = new ObjectMapper().valueToTree(patient).toString();
+
+        when(patientRepository.findOneByDocument(patient.getDocument())).thenReturn(Optional.empty());
+        when(patientRepository.findOneByEmail(patient.getEmail())).thenReturn(Optional.empty());
+        when(patientRepository.save(patient)).thenReturn(patient);
+
+        this.mockMvc.perform(post("/patient/create")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 
     @Test
-    void shouldReturnDefaultMessage() throws Exception {
-        //when(patientService.message()).thenReturn("Hello, mock");
-        this.mockMvc.perform(get("/patient/"))
+    void shouldThrowDocumentInvalidExceptionCreate() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857002");
+        patient.setEmail("emailTest@test.com");
+
+        String requestBody = new ObjectMapper().valueToTree(patient).toString();
+
+        this.mockMvc.perform(post("/patient/create")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("ping")));
+                .andExpect(status().isBadRequest());
+    }
+    @Test
+    void shouldThrowDocumentConflictExceptionCreate() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857003");
+        patient.setEmail("emailTest@test.com");
+
+        Patient patient2 = new Patient();
+        patient2.setId(Long.valueOf(2));
+        patient2.setDocument("95105857003");
+        patient2.setEmail("emailTest@test.com");
+
+        String requestBody = new ObjectMapper().valueToTree(patient).toString();
+
+        when(patientRepository.findOneByDocument(patient.getDocument())).thenReturn(Optional.of(patient2));
+        this.mockMvc.perform(post("/patient/create")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    void shouldThrowEmailConflictExceptionCreate() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857003");
+        patient.setEmail("emailTest@test.com");
+
+        Patient patient2 = new Patient();
+        patient2.setId(Long.valueOf(2));
+        patient2.setDocument("95105857003");
+        patient2.setEmail("emailTest@test.com");
+
+        String requestBody = new ObjectMapper().valueToTree(patient).toString();
+
+        when(patientRepository.findOneByEmail(patient.getEmail())).thenReturn(Optional.of(patient2));
+        this.mockMvc.perform(post("/patient/create")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+    // Update Tests
+    @Test
+    void shouldUpdatePatient() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857003");
+        patient.setEmail("emailTest@test.com");
+
+        String requestBody = new ObjectMapper().valueToTree(patient).toString();
+
+        when(patientRepository.findOneByDocument(patient.getDocument())).thenReturn(Optional.of(patient));
+        when(patientRepository.findOneByEmail(patient.getEmail())).thenReturn(Optional.of(patient));
+        when(patientRepository.save(patient)).thenReturn(patient);
+
+        this.mockMvc.perform(post("/patient/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldThrowDocumentInvalidExceptionUpdate() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857002");
+        patient.setEmail("emailTest@test.com");
+
+        String requestBody = new ObjectMapper().valueToTree(patient).toString();
+
+        this.mockMvc.perform(post("/patient/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+    @Test
+    void shouldThrowDocumentConflictExceptionUpdate() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857003");
+        patient.setEmail("emailTest@test.com");
+
+        Patient patient2 = new Patient();
+        patient2.setId(Long.valueOf(2));
+        patient2.setDocument("95105857003");
+        patient2.setEmail("emailTest@test.com");
+
+        String requestBody = new ObjectMapper().valueToTree(patient).toString();
+
+        when(patientRepository.findOneByDocument(patient.getDocument())).thenReturn(Optional.of(patient2));
+        this.mockMvc.perform(post("/patient/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    void shouldThrowEmailConflictExceptionUpdate() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857003");
+        patient.setEmail("emailTest@test.com");
+
+        Patient patient2 = new Patient();
+        patient2.setId(Long.valueOf(2));
+        patient2.setDocument("95105857003");
+        patient2.setEmail("emailTest@test.com");
+
+        String requestBody = new ObjectMapper().valueToTree(patient).toString();
+
+        when(patientRepository.findOneByEmail(patient.getEmail())).thenReturn(Optional.of(patient2));
+        this.mockMvc.perform(post("/patient/update")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+
+    // Remove Tests
+    @Test
+    void shouldRemovePatient() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857003");
+        patient.setEmail("emailTest@test.com");
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.of(patient));
+        this.mockMvc.perform(delete("/patient/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldNotRemovePatientNotfound() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857003");
+        patient.setEmail("emailTest@test.com");
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.empty());
+        this.mockMvc.perform(delete("/patient/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    // Get Tests
+    @Test
+    void shouldGetPatient() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857003");
+        patient.setEmail("emailTest@test.com");
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.of(patient));
+        this.mockMvc.perform(get("/patient/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldNotGetPatientNotfound() throws Exception {
+        Patient patient = new Patient();
+        patient.setId(Long.valueOf(1));
+        patient.setDocument("95105857003");
+        patient.setEmail("emailTest@test.com");
+
+        when(patientRepository.findById(patient.getId())).thenReturn(Optional.empty());
+        this.mockMvc.perform(get("/patient/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
 }
